852e638052436bab2517e4728d6af9ce0e2bd41f
Parker Snell <parkersnell@gmail.com>
2021-04-19 13:09:45 -0700

    organize tests

---
+++ test/generator.cpp
@@ -0,0 +1,84 @@
+#define WSCORO_LOG g_logger
+#include "wscoro/task.h"
+
+#include <catch2/catch_all.hpp>
+
+#include <sstream>
+
+using namespace wscoro;
+
+static Generator<int> fibonacci() {
+  int a0 = 1;
+  int a1 = 1;
+  while (true) {
+    co_yield a0;
+    co_yield a1;
+    a0 += a1;
+    a1 += a0;
+  }
+}
+
+static AsyncGenerator<int> async_fib() {
+  auto fib = fibonacci();
+  while (true) {
+    co_yield *co_await fib;
+  }
+}
+
+template<typename G>
+static AutoTask<std::string> get_seq(G generator, int rounds) {
+  std::stringstream ss;
+  if (rounds > 0) {
+    for (int i = 0; i < rounds - 1; i++) {
+      ss << *std::move(co_await generator) << " ";
+    }
+    ss << *co_await generator;
+  }
+  co_return std::move(ss).str();
+}
+
+TEST_CASE("Fibonacci generator (not called)", "[generator]") {
+  auto fib_seq = get_seq(fibonacci(), 0);
+  CHECK(fib_seq.await_ready()); REQUIRE(fib_seq.await_resume() == "");
+}
+
+TEST_CASE("Fibonacci generator (called once)", "[generator]") {
+  auto fib_seq = get_seq(fibonacci(), 1);
+  CHECK(fib_seq.await_ready());
+  REQUIRE(fib_seq.await_resume() == "1");
+}
+
+TEST_CASE("Fibonacci generator", "[generator]") {
+  auto fib_seq = get_seq(fibonacci(), 7);
+  CHECK(fib_seq.await_ready());
+  REQUIRE(fib_seq.await_resume() == "1 1 2 3 5 8 13");
+}
+
+TEST_CASE("Fibonacci without co_await", "[generator]") {
+  auto fib = fibonacci();
+  const int seq[] = {1, 1, 2, 3, 5};
+
+  for (int i = 0; i < sizeof(seq)/sizeof(seq[0]); ++i) {
+    fib.resume();
+    CHECK(!fib.done());
+    REQUIRE(fib.await_resume() == seq[i]);
+  }
+}
+
+TEST_CASE("Async generator (not called)", "[generator]") {
+  auto fib_seq = get_seq(async_fib(), 0);
+  CHECK(fib_seq.await_ready());
+  REQUIRE(fib_seq.await_resume() == "");
+}
+
+TEST_CASE("Async generator (called once)", "[generator]") {
+  auto fib_seq = get_seq(async_fib(), 1);
+  CHECK(fib_seq.await_ready());
+  REQUIRE(fib_seq.await_resume() == "1");
+}
+
+TEST_CASE("Async generator", "[generator]") {
+  auto fib_seq = get_seq(async_fib(), 7);
+  CHECK(fib_seq.await_ready());
+  REQUIRE(fib_seq.await_resume() == "1 1 2 3 5 8 13");
+}
---
+++ test/suspend.cpp
@@ -0,0 +1,79 @@
+#define WSCORO_LOG g_logger
+#include "wscoro/task.h"
+
+#include <catch2/catch_all.hpp>
+
+#include <sstream>
+
+using namespace wscoro;
+
+template<typename TaskB>
+TaskB suspend_b(std::stringstream &s) {
+  s << ", [B0]";
+  co_await std::suspend_always{};
+  s << "B1";
+}
+
+template<typename TaskA, typename TaskB>
+TaskA suspend_a(std::stringstream &s, TaskB &b) {
+  s << "A0";
+  b = suspend_b<TaskB>(s);
+  s << ", A1]";
+  co_await b;
+  s << ", [A2";
+}
+
+// dosn't get the continuation
+template<typename TaskA, typename TaskB>
+void test_suspend_step(TaskA &ta, int sa, TaskB &tb, int sb,
+                       std::stringstream &s) {
+  const auto loop = [&s](auto &t, int st, auto a_done) {
+    bool a_once = false;
+    for (int i = 0; i < st; ++i) {
+      CHECK(!t.done());
+      t.resume();
+      if (a_done() && !a_once) {
+        a_once = true;
+        s << ", AF]";
+      }
+    }
+  };
+
+  loop(ta, sa, [] { return false; });
+  if (tb) {
+    s << ", B: [";
+    loop(tb, sb, [&] { return ta.done(); });
+    CHECK(tb.done());
+    s << ", BF]";
+  } else if (sb > 0) {
+    FAIL_CHECK("expected task tb with " << sb << " suspensions");
+  }
+  CHECK(ta.done());
+}
+
+template<typename TaskA, typename TaskB>
+void test_suspend(int steps_a, int steps_b, const char *expected) {
+  std::stringstream s;
+  // Extend the lifetime of this task so we can verify when it finishes.
+  TaskB b{nullptr};
+  s << "A: [";
+  auto a = suspend_a<TaskA, TaskB>(s, b);
+  test_suspend_step(a, steps_a, b, steps_b, s);
+  REQUIRE(s.str() == expected);
+}
+
+TEST_CASE("Suspension", "[task]") {
+  test_suspend<
+    AutoTask<>, AutoTask<>>(0, 1,
+    "A: [A0, [B0], A1], B: [B1, [A2, AF], BF]");
+  test_suspend<
+    Task<>, AutoTask<>>(1, 1,
+    "A: [A0, [B0], A1], B: [B1, [A2, AF], BF]");
+
+  test_suspend<
+    AutoTask<>, Task<>>(0, 1,
+    "A: [A0, A1], [B0], B: [B1, [A2, AF], BF]");
+  test_suspend<
+    Task<>, Task<>>(1, 1,
+    "A: [A0, A1], [B0], B: [B1, [A2, AF], BF]");
+}
